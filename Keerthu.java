753a1c8cf6c09af57489a839c27920714f6c34f7
GokulSGK <gokul.dinesh1997@gmail.com>
2017-04-18 22:12:18 +0530

    Update Keerthu.java

--- Keerthu.java
+++ Keerthu.java
@@ -1,1 +1,15 @@
-
+import java.util.*;
+public class Keerthu
+{
+  public static void main(String args[])
+  {
+    int a;
+    a=Integer.parseInt(args[0]);
+    if(a<0)
+      System.out.println("Negative");
+    else if(a>0)
+      System.out.println("Positive");
+    else
+      System.out.println("Zero");
+  }
+}  
