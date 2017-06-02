66eacdcfd1294018b6eb09086729b7e054b3e1ac
GokulSGK <gokul.dinesh1997@gmail.com>
2017-06-02 16:04:07 +0530

    Create unique.java

---
+++ unique.java
@@ -0,0 +1,31 @@
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+import org.apache.commons.lang.ArrayUtils;
+
+public class unique {
+    public static void main(String args[])
+    {
+         int a[] = new int[5];
+        Scanner s = new Scanner(System.in);
+        int i,j;
+        for(i=0;i<5;i++)
+        {
+            a[i] = s.nextInt();
+        }
+        for(i=0;i<a.length;i++)
+	{
+	for(j=1;j<a.length;j++)
+	    {
+		if(a[i] == a[j])
+		{
+			Arrays.toString(a);
+                        a = ArrayUtils.remove(a,i);
+                        a = ArrayUtils.remove(a,j);
+		}
+		}}
+        System.out.print(a);
+    }
+    
+}
