977b239e0dd7a2470b9f03f6e9273ba8c39cbb02
GokulSGK <gokul.dinesh1997@gmail.com>
2017-06-07 15:53:34 +0530

    Update Alphabet.java

--- Alphabet.java
+++ Alphabet.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
 
-public class Alphabet
-{
-    public static void main(String args[])
-    {
-        char ch;
-        Scanner scan = new Scanner(System.in);
-		
-        System.out.print("Enter a Character : ");
-        ch = scan.next().charAt(0);
-		
-        if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))
-        {
-            System.out.print(ch + " is an Alphabet");
-        }
-        else
-        {
-            System.out.print(ch + " is not an Alphabet");
-        }
-    }
+public class Alphabet {
+
+	public static void main(String[] args) {
+		char c;
+		int k=0;
+		Scanner s = new Scanner(System.in);
+		c = s.next().charAt(0);
+		if(c>='a' && c<='z' || c>='A' && c<='Z')
+		{
+			k++;
+		}
+		if(k!=0)
+		{
+			System.out.print("Alphabet");
+			
+		}
+		else
+		{
+			System.out.print("Not an Alphabet");
+		}
+
+	}
+
 }
