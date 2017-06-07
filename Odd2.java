58bc0da1c3759aff3849752be625e438483dcdd9
GokulSGK <gokul.dinesh1997@gmail.com>
2017-06-07 18:01:25 +0530

    Create Odd2.java

---
+++ Odd2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Odd2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num[]=new int[10];
+		Scanner s = new Scanner(System.in);
+		for(int i=0;i<num.length;i++)
+		{
+		num[i] = s.nextInt();
+		}
+		System.out.println("Define the limits");
+		int a,b;
+		a = s.nextInt();
+		b = s.nextInt();
+		for(int i=a;i<b-1;i++)
+		{
+		if(num[i]%2==0)
+		{
+			System.out.println(num[i]+"IS EVEN");
+		}
+		}	
+
+	}
+
+}
